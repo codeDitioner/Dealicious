60f072c66d509d379f82f12c3df52831aabc9874
codeDitioner <nguyennkevin@gmail.com>
2022-10-31 22:40:19 -0400

    Update

---
+++ Dealicious/Dealicious/ProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileViewController.swift
+//  Dealicious
+//
+//  Created by Kevin Nguyen on 10/31/22.
+//  Copyright © 2022 Mina Sedhom. All rights reserved.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class ProfileViewController: UIViewController {
+    @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var userDescription: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
