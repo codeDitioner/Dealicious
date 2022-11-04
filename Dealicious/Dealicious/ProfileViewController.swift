89b09408af468a7e948d1554449bc0c6e22c7fca
codeDitioner <nguyennkevin@gmail.com>
2022-11-03 20:57:33 -0400

    Updated edit profile and my profile view controllers.

--- Dealicious/Dealicious/ProfileViewController.swift
+++ Dealicious/Dealicious/ProfileViewController.swift
@@ -14,10 +14,20 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var userLabel: UILabel!
     @IBOutlet weak var userDescription: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let description = PFUser.current()!["aboutMe"] as? String {
+            userDescription.text = description
+        }
+        if let imageFile = PFUser.current()!["image"] as? PFFileObject {
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            profileImage.af.setImage(withURL: url)
+        }
+        if let username = PFUser.current()!["username"] as? String {
+            userLabel.text = username
+        }
         // Do any additional setup after loading the view.
     }
     
